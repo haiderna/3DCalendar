1ea713e3578b6039b39036047237c188be409261
Najia Haider <haiderna@vcu.edu>
2019-02-21 13:37:34 -0500

    Paste CalendarView onto node

---
+++ 3DCalendar/CalendarView.swift
@@ -0,0 +1,166 @@
+//
+//  CalendarView.swift
+//  bolt
+//
+//  Created by Najia Haider on 2/20/19.
+//  Copyright © 2019 Najia Haider. All rights reserved.
+//
+
+import UIKit
+
+class CalendarView: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+ 
+    var numDaysPerMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+    var currentMonthIndex: Int = 0
+    var currentYear: Int = 0
+    var presentMonthIndex = 0
+    var presentYear = 0
+    var todaysDate = 0
+    var firstWeekDayOfMonth = 0
+    
+    let myCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let myCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        myCollectionView.showsHorizontalScrollIndicator = false
+        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        myCollectionView.backgroundColor = .clear
+        myCollectionView.allowsMultipleSelection = false
+        return myCollectionView
+        
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initializeView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initializeView() {
+        currentMonthIndex = Calendar.current.component(.month, from: Date())
+        currentYear = Calendar.current.component(.year, from: Date())
+        todaysDate = Calendar.current.component(.day, from: Date())
+        //firstWeekDayOfMonth = getFirstWeekDay()
+        
+        //leap years
+        if currentMonthIndex == 2 && currentYear % 4 == 0 {
+            numDaysPerMonth[currentMonthIndex-1] = 29
+        }
+        
+        presentMonthIndex = currentMonthIndex
+        presentYear = currentYear
+        
+        setUpViews()
+        
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        myCollectionView.register(dateCell.self, forCellWithReuseIdentifier: "Cell")
+        
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+   
+    func getFirstWeekDay() -> Int {
+        let day = ("\(currentYear)-\(currentMonthIndex)-01".date?.firstDayOfTheMonth.weekday)!
+        //return day == 7 ? 1 : day
+        return day
+    }
+    
+    func setUpViews() {
+        addSubview(myCollectionView)
+        myCollectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        myCollectionView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        myCollectionView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        myCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numDaysPerMonth[currentMonthIndex-1] + firstWeekDayOfMonth - 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! dateCell
+        cell.backgroundColor=UIColor.clear
+        if indexPath.item <= firstWeekDayOfMonth - 2 {
+            cell.isHidden = true
+        } else {
+            let calcDate = indexPath.row-firstWeekDayOfMonth+2
+            cell.isHidden = false
+            cell.label.text="\(calcDate)"
+            if calcDate < todaysDate && currentYear == presentYear && currentMonthIndex == presentMonthIndex {
+                cell.isUserInteractionEnabled=false
+                cell.label.textColor = UIColor.lightGray
+            } else {
+                cell.isUserInteractionEnabled=true
+                cell.label.textColor = UIColor.black
+            }
+        }
+        return cell
+    }
+
+}
+
+class dateCell: UICollectionViewCell {
+    
+    let label: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "00"
+        lbl.textAlignment = .center
+        lbl.font = UIFont.systemFont(ofSize: 16)
+        lbl.textColor = UIColor.gray
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews() {
+        addSubview(label)
+        label.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        label.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        label.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+}
+
+extension Date {
+    var weekday: Int {
+        return Calendar.current.component(.weekday, from: self)
+    }
+    var firstDayOfTheMonth: Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year,.month], from: self))!
+    }
+}
+
+extension String {
+    static var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    var date: Date? {
+        return String.dateFormatter.date(from: self)
+    }
+}
--- 3DCalendar/ViewController.swift
+++ 3DCalendar/ViewController.swift
@@ -23,11 +23,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        createUIViewOnNode()
         // Create a new scene
-        let scene = SCNScene(named: "dodecahedron.scnassets/dodecahedron.scn")!
+        //let scene = SCNScene(named: "dodecahedron.scnassets/dodecahedron.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+       // sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +49,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    func shapeNode() {
+        let scene = SCNScene(named: "dodecahedron.scnassets/dodecahedron.scn")!
+        let node = scene.rootNode.childNode(withName: "dodecahedron", recursively: true)
+        
+    }
+    
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -72,4 +80,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    //pentagon via Bezier Path
+    func createPentagon() -> SCNNode {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0.0, y: 0.6)) //A
+        path.addLine(to: CGPoint(x: -0.3, y: 0.4))//B
+        path.addLine(to: CGPoint(x: -0.2, y: 0.1))//C
+        path.addLine(to: CGPoint(x: 0.2, y: 0.1))//D
+        path.addLine(to: CGPoint(x: 0.3, y: 0.4))//E
+        path.close()
+        
+        let shape = SCNShape(path: path, extrusionDepth: 0.2) //20 cm
+        let color = UIColor.red
+        shape.firstMaterial?.diffuse.contents = color
+        shape.chamferRadius = 0.1
+        //create a node
+        let pentagonNode = SCNNode(geometry: shape)
+        //position the node and add to the scene
+        pentagonNode.position.z = -1
+        return pentagonNode
+    }
+    
+    //Test UI paste on top of node
+    func createUIViewOnNode(){
+        
+        //1. Create An Empty Node
+        let holderNode = createPentagon()
+        
+        
+        //2. Create A New Material
+        let material = SCNMaterial()
+        
+        //3. Create A UIView As A Holder For Content
+        let viewToAdd = CalendarView(frame: CGRect(x: 100, y: 100, width: 500, height: 500))
+        
+        //5. Set The Materials Contents
+        material.diffuse.contents = viewToAdd
+        
+        //6. Set The 1st Material Of The Plane
+        holderNode.geometry?.firstMaterial = material
+        material.isDoubleSided = true
+        //holderNode.addChildNode(node)
+        
+        //7. Add To The Scene & Position It
+        sceneView.scene.rootNode.addChildNode(holderNode)
+        
+        holderNode.position = SCNVector3(0, 0, -1.0)
+    }
+    
 }
